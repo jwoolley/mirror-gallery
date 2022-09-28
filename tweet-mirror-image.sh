ae9b7831c7ffce9ee666f787b11827d15e9c81ff
Justin Woolley <jwoolley@users.noreply.github.com>
2022-09-27 23:57:39 -0700

    mirror image script

---
+++ tweet-mirror-image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#noisy
+set -x
+
+#log the path
+echo $PATH
+
+#log node version
+node -v
+
+# install fonts
+cp ./src/data/fonts/*.ttf /usr/local/share/fonts
+cp ./src/data/fonts/*.otf /usr/local/share/fonts
+
+# install dependencies
+npm install --production
+
+# tweet
+
+if
+  [ -z "$1" ]
+then
+	echo "generating mirror gallery image"
+	node --trace-deprecation --harmony mtgnews -o "#flippedCardImage#"
+else
+	echo "generating $1 mirror gallery image(s)"
+	node --trace-deprecation --harmony mtgnews -c $1 -o "#flippedCardImage#"
+fi
+# node --harmony mtgnews -o "#flippedCardImage#" # --tweet --toot --discord
+
+#silent
+set +x
