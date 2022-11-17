11d782fb6bc6dff7692fb157738a2ff3c3911392
Justin Woolley <jwoolley@users.noreply.github.com>
2022-11-16 19:43:40 -0800

    bash script for server start

---
+++ run_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GIT_BRANCH="express-server"
+EXPRESS_SERVER_PORT=80
+# update source
+git fetch origin $GIT_BRANCH
+git pull --rebase origin $GIT_BRANCH
+
+npm install
+PORT=$EXPRESS_SERVER_PORT npm run server
