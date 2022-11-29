79ff1a7865243616f91a5220a70e39593aa4444f
Justin Woolley <jwoolley@users.noreply.github.com>
2022-11-29 12:14:33 -0800

    grammar-cleanup

--- src/lib/plugins/tracery/modifiers/index.js
+++ src/lib/plugins/tracery/modifiers/index.js
@@ -5,8 +5,6 @@ const deepMerge = require(`../../../util/deep-merge.js`);
 let modifiers = {};
 
 modifiers = deepMerge(modifiers, require('./basic-modifiers'));
-modifiers = deepMerge(modifiers, require('./cardfinder-modifiers'));
 modifiers = deepMerge(modifiers, require('./content-modifiers'));
-modifiers = deepMerge(modifiers, require('./scryfall-modifiers'));
 
 module.exports = modifiers;
