d1493427ce4ce356bf91d053df121467b0bc1e9b
Justin Woolley <jwoolley@users.noreply.github.com>
2024-01-05 00:39:26 +0000

    pm2 config file; update run_server script to support fresh installs

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [{
+    name   : "run_server",
+    script : "./run_server.sh",
+    interpreter: "/usr/bin/bash"
+  }]
+}
--- run_server.sh
+++ run_server.sh
@@ -5,7 +5,9 @@ EXPRESS_SERVER_PORT=80
 
 # update source
 git fetch origin $GIT_BRANCH
+git reset --hard origin/$GIT_BRANCH
 git pull --rebase origin $GIT_BRANCH
+git checkout $GIT_BRANCH
 
 npm install
 PORT=$EXPRESS_SERVER_PORT npm run server
