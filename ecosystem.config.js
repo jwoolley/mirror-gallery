dcb512079b2ec2a5c88f25302f5ecfa428b189f3
Justin Woolley <jwoolley@users.noreply.github.com>
2024-01-05 18:18:07 +0000

    add watch params to pm config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps : [{
     name   : "run_server",
     script : "./run_server.sh",
-    interpreter: "/usr/bin/bash"
+    interpreter: "/usr/bin/bash",
+    ignore_watch: ["node_modules", ".git"],
+    watch_delay: 1000
   }]
 }
