3750305fd4ef0a7c1856357971972c7bc594f3a8
Justin Woolley <jwoolley@users.noreply.github.com>
2024-01-05 01:26:49 +0000

    disable git code pull in run_server script

--- run_server.sh
+++ run_server.sh
@@ -3,11 +3,13 @@
 GIT_BRANCH="express-server"
 EXPRESS_SERVER_PORT=80
 
+# DISABLED SINCE THIS IS MANAGED BY GITHUB ACTIONS
 # update source
-git fetch origin $GIT_BRANCH
-git reset --hard origin/$GIT_BRANCH
-git pull --rebase origin $GIT_BRANCH
-git checkout $GIT_BRANCH
+# git fetch origin $GIT_BRANCH
+# git reset --hard origin/$GIT_BRANCH
+# git pull --rebase origin $GIT_BRANCH
+# git checkout $GIT_BRANCH
+# code is copied to /opt/
 
 npm install
 PORT=$EXPRESS_SERVER_PORT npm run server
